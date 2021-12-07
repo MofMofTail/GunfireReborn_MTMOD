61062bf90f3327873a5af57eca40f7bd4dc4ae39
ReshiremSato <redorumon@hotmail.com>
2021-12-07 16:02:42 +0800

    delete super weapon and update somethings

--- GunfireReborn_MTMOD/Main.cs
+++ GunfireReborn_MTMOD/Main.cs
@@ -2,6 +2,7 @@
 using Il2CppSystem.Collections.Generic;
 using MelonLoader;
 using UnityEngine;
+
 namespace GunfireReborn_MTMOD
 {
     public static class BuildInfo
@@ -10,93 +11,79 @@ namespace GunfireReborn_MTMOD
         public const string Description = "GunfireReborn Mod "; // Description for the Mod. (Set as null if none)
         public const string Author = "MT"; // Author of the Mod. (MUST BE SET)
         public const string Company = null; // Company that made the Mod. (Set as null if none)
-        public const string Version = "1.0.0"; // Version of the Mod. (MUST BE SET)
+        public const string Version = "1.0.1"; // Version of the Mod. (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod. (Set as null if none)
     }
+
     public class MTMOD : MelonMod
     {
-        public static bool Infbullets = false;
-        public static bool weaponmod = false;
-        public static bool caidan = true;
+        public static bool InfBullets;
+        public static bool Menu = true;
 
 
         public override void OnApplicationStart() // Runs after Game Initialization.
         {
             MelonLogger.Msg("GunfireReborn_MTMOD Loder");
         }
+
         public override void OnUpdate() // Runs once per frame.
         {
             try
             {
-                if ((MTMOD.Infbullets || MTMOD.weaponmod || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow)))
+                if (InfBullets || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) ||
+                    Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
                 {
-                    if (HeroCameraManager.HeroObj != null && HeroCameraManager.HeroObj.BulletPreFormCom != null && HeroCameraManager.HeroObj.BulletPreFormCom.weapondict != null)
+                    if (HeroCameraManager.HeroObj != null && HeroCameraManager.HeroObj.BulletPreFormCom != null &&
+                        HeroCameraManager.HeroObj.BulletPreFormCom.weapondict != null)
                         foreach (var weapon in HeroCameraManager.HeroObj.BulletPreFormCom.weapondict)
                         {
-                            if (MTMOD.Infbullets)
+                            if (InfBullets)
                             {
                                 weapon.value.ModifyBulletInMagzine(200, 200);
                                 weapon.value.ReloadBulletImmediately();
                             }
-                            if (MTMOD.weaponmod)
-                            {
-                                List<int> list = weapon.value.WeaponAttr.Accuracy;
-                                list[0] = 100000;
-                                weapon.value.WeaponAttr.Accuracy = list;//武器精确度
-                                weapon.value.WeaponAttr.Stability = list;//武器稳定性
-                                // weapon.value.WeaponAttr.MaxBullet = 200;//弹夹最大子弹数目
-                                weapon.value.WeaponAttr.AttDis = 9999f;//射击距离
-                                weapon.value.WeaponAttr.FillTime = 5;//换弹时间
 
-                                List<int> attSpeed = weapon.value.WeaponAttr.AttSpeed;
-                                attSpeed[0] = weapon.value.WeaponAttr.AttSpeed[0] + 1000;//部分武器只会很鬼畜但攻速没加多少
-                                weapon.value.WeaponAttr.AttSpeed = attSpeed;
-
-                                if (weapon.value.WeaponAttr.BulletSpeed >= 50f && weapon.value.WeaponAttr.BulletSpeed != 55f || weapon.value.WeaponAttr.BulletSpeed == 30f)
-                                {
-                                    weapon.value.WeaponAttr.BulletSpeed = 500;//弹道速度
-                                }
-
-                            }
                             if (Input.GetKeyUp(KeyCode.UpArrow))
                             {
                                 weapon.value.WeaponAttr.Radius += 5f;
                             }
+
                             if (Input.GetKeyUp(KeyCode.DownArrow))
                             {
                                 weapon.value.WeaponAttr.Radius -= 5f;
                             }
                         }
                 }
+
                 if (Input.GetKeyUp(KeyCode.F1))
                 {
-                    MTMOD.Infbullets = !MTMOD.Infbullets;
+                    InfBullets = !InfBullets;
                 }
-                if (Input.GetKeyUp(KeyCode.F2))
-                {
-                    MTMOD.weaponmod = !MTMOD.weaponmod;
-                }
+
                 if (Input.GetKeyUp(KeyCode.Home))
                 {
-                    MTMOD.caidan = !MTMOD.caidan;
+                    Menu = !Menu;
                 }
+
                 if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
-                    HeroCameraManager.HeroObj.playerProp.Speed += 100;
+                    if (HeroCameraManager.HeroObj != null) HeroCameraManager.HeroObj.playerProp.Speed += 100;
                 }
+
                 if (Input.GetKeyDown(KeyCode.LeftArrow))
                 {
-                    HeroCameraManager.HeroObj.playerProp.Speed -= 100;
+                    if (HeroCameraManager.HeroObj != null) HeroCameraManager.HeroObj.playerProp.Speed -= 100;
                 }
+
                 if (Input.GetKeyDown(KeyCode.Keypad9))
                 {
                     HeroMoveManager.HMMJS.jumping.baseHeight += 0.2f;
                 }
+
                 if (Input.GetKeyDown(KeyCode.Keypad3))
                 {
                     HeroMoveManager.HMMJS.jumping.baseHeight -= 0.2f;
                 }
-               
 
 
                 if (Input.GetKey(KeyCode.F))
@@ -106,24 +93,27 @@ namespace GunfireReborn_MTMOD
                     {
                         Vector3 campos = CameraManager.MainCamera.position;
                         Transform nearmons = null;
-                        Transform monsterTransform = null;
+                        Transform monsterTransform;
                         float neardis = 500f;
                         foreach (var monster in monsters)
                         {
-                            if ((double)monster.BloodBarCom.BloodBar.hp >= 0.05)
+                            if (monster.BloodBarCom.BloodBar.hp >= 0.05)
                             {
-                                monsterTransform = monster.BodyPartCom.GetWeakTrans(false);
+                                monsterTransform = monster.BodyPartCom.GetWeakTrans();
                                 if (monsterTransform == null) continue;
-                                Vector3 vector = CameraManager.MainCameraCom.WorldToViewportPoint(monsterTransform.position);
+                                Vector3 vector =
+                                    CameraManager.MainCameraCom.WorldToViewportPoint(monsterTransform.position);
                                 bool flag = false;
-                                if (vector.x >= 0.45f && vector.x <= 0.55f && vector.y >= 0.45f && vector.y <= 0.55f && vector.z > 0f)
+                                if (vector.x >= 0.45f && vector.x <= 0.55f && vector.y >= 0.45f && vector.y <= 0.55f &&
+                                    vector.z > 0f)
                                 {
                                     vector.y = 0f;
                                     vector.x = 0.5f - vector.x;
-                                    vector.x = (float)Screen.width * vector.x;
+                                    vector.x = Screen.width * vector.x;
                                     vector.z = 0f;
                                     flag = true;
                                 }
+
                                 if (flag)
                                 {
                                     vector = monsterTransform.position - campos;
@@ -133,12 +123,15 @@ namespace GunfireReborn_MTMOD
                                     bool visible = true;
                                     foreach (RaycastHit raycastHit in hits)
                                     {
-                                        if (raycastHit.collider.gameObject.layer == 0 || raycastHit.collider.gameObject.layer == 30 || raycastHit.collider.gameObject.layer == 31)
+                                        if (raycastHit.collider.gameObject.layer == 0 ||
+                                            raycastHit.collider.gameObject.layer == 30 ||
+                                            raycastHit.collider.gameObject.layer == 31)
                                         {
                                             visible = false;
                                             break;
                                         }
                                     }
+
                                     if (visible && curdis < neardis)
                                     {
                                         neardis = curdis;
@@ -147,17 +140,20 @@ namespace GunfireReborn_MTMOD
                                 }
                             }
                         }
+
                         if (nearmons != null)
                         {
                             Vector3 objpos = new Vector3();
-                            objpos.x = HeroCameraManager.HeroObj.gameTrans.position.x;
-                            objpos.y = nearmons.position.y + 0.2f;
-                            objpos.z = HeroCameraManager.HeroObj.gameTrans.position.z;
-                            Vector3 forward = nearmons.position - objpos;
+                            var heroPosition = HeroCameraManager.HeroObj.gameTrans.position;
+                            var nearmonsPosition = nearmons.position;
+                            objpos.x = heroPosition.x;
+                            objpos.y = nearmonsPosition.y + 0.2f;
+                            objpos.z = heroPosition.z;
+                            Vector3 forward = nearmonsPosition - objpos;
                             forward.y += 0.12f;
                             Quaternion rotation = Quaternion.LookRotation(forward);
                             HeroCameraManager.HeroObj.gameTrans.rotation = rotation;
-                            forward = nearmons.position - campos;
+                            forward = nearmonsPosition - campos;
                             forward.y += 0.12f;
                             Quaternion rotation2 = Quaternion.LookRotation(forward);
                             CameraManager.MainCamera.rotation = rotation2;
@@ -167,28 +163,28 @@ namespace GunfireReborn_MTMOD
             }
             catch
             {
+                // ignored
             }
         }
-        
+
         public override void OnGUI() // Can run multiple times per frame. Mostly used for Unity's IMGUI.
         {
-
             try
             {
-                if (MTMOD.caidan)
+                if (Menu)
                 {
                     int GUIY = 400;
-                    GUI.Label(new Rect(30f, (float)GUIY, 150f, 20f), MTMOD.caidan ? "Home 菜单: 开启" : "Home 菜单: 关闭");
+                    GUI.Label(new Rect(30f, (float) GUIY, 150f, 20f), MTMOD.Menu ? "Home 菜单: 开启" : "Home 菜单: 关闭");
                     GUIY += 20;
-                    GUI.Label(new Rect(30f, (float)GUIY, 300, 20f), "←→ 加减移动速度,↑↓加减爆炸范围");
+                    GUI.Label(new Rect(30f, (float) GUIY, 300, 20f), "←→ 加减移动速度,↑↓加减爆炸范围");
                     GUIY += 20;
-                    GUI.Label(new Rect(30f, (float)GUIY, 150f, 20f), MTMOD.Infbullets ? "F1 无限子弹: 开启" : "F1 无限子弹: 关闭");
-                    GUIY += 20;
-                    GUI.Label(new Rect(30f, (float)GUIY, 150f, 20f), MTMOD.weaponmod ? "F2 武器改装: 开启" : "F2 武器改装: 关闭");
+                    GUI.Label(new Rect(30f, (float) GUIY, 150f, 20f), MTMOD.InfBullets ? "F1 无限子弹: 开启" : "F1 无限子弹: 关闭");
                 }
             }
-            catch { }
+            catch
+            {
+                // ignored
+            }
         }
-
     }
 }
